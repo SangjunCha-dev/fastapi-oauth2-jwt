0a50ed0d792645345eeabd4a006cddf12621f64c
SangjunCha <SangjunCha.dev@gmail.com>
2022-07-06 11:15:00 +0900

    ✅ Add create_item

--- app/tests/api/test_items.py
+++ app/tests/api/test_items.py
@@ -1,9 +1,31 @@
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
-from app.core.config import settings
 from app.tests.utils.items import create_random_item
 
+
+def test_create_item(
+    client: TestClient,
+    superuser_token_headers: dict,
+) -> None:
+    data = {
+        "name": "product1",
+        "description": "product_description1",
+        "price": 1000,
+        "quantity": 10,
+    }
+    response = client.post("/items", headers=superuser_token_headers, json=data)
+
+    assert response.status_code == 200
+
+    content = response.json()
+
+    assert "id" in content
+    assert content["name"] == data["name"]
+    assert content["description"] == data["description"]
+    assert content["price"] == data["price"]
+    assert content["quantity"] == data["quantity"]
+    assert "owner_id" in content
 
 
 def test_read_item(
